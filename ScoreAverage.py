27b4643a6280b07ea21a5abe037af89eac9d119b
Jacashin <jacobashin@gmail.com>
2023-12-30 21:35:02 -0500

    Just hacking around with Python

---
+++ ScoreAverage.py
@@ -0,0 +1,16 @@
+print("The test scores program\n")
+print("Enter 3 test scores")
+print("============")
+
+total_score = 0
+total_score += int(input("Enter 1: "))
+total_score += int(input("Enter 2: "))
+total_score += int(input("Enter 3: "))
+
+average_score = round(total_score /3)
+
+print("total score:  ", total_score, "\nAverage Score:  ", average_score)
+
+print()
+print("Good bye")
+
